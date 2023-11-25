40807583540f461470a9cc8a4593d6db764d84ba
hideto tsuzuki <ma_kux@icloud.com>
2023-11-25 16:40:48 +0900

    年収管理表画面実装中

--- src/views/AnnualIncomeManagement.vue
+++ src/views/AnnualIncomeManagement.vue
@@ -73,10 +73,112 @@
         </v-card>
       </v-menu>
     </div>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :sort-by="[{ key: 'calories', order: 'asc' }]"
+    >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>年収管理表</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ props }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="props"
+            >
+              新規登録
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                  <v-text-field 
+                    v-for="(item) in annualIncomeList" :key="item.label"
+                    v-model="item.vModel"
+                    :label="item.label"
+                    type="number | string"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="close"
+              >
+                キャンセル
+              </v-btn>
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="save"
+              >
+                登録
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        size="small"
+        class="me-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        size="small"
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+  </v-data-table>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, computed } from 'vue'
+import { ref, Ref, onMounted, computed, watch, nextTick } from 'vue'
 import ApiEndpoint from "../common/apiEndpoint"
 import Datepicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
@@ -88,16 +190,134 @@ const endDate = ref<Date>()
 const tmpStartDate = ref<Date>()
 const tmpEndDate = ref<Date>()
 const screenFlag = ref<boolean>(false)
+// 仮状態の期間を変更した／していないのフラグ
+const isMenuOpened = ref<boolean>(false)
+const isTmpStartDateChanged = ref<boolean>(false)
+const isTmpEndDateChanged = ref<boolean>(false)
+// メニューが表示されているなら true
+const paymentDateValue = ref<string>('')
+const ageValue = ref<string>('')
+const industryValue = ref<string>('')
+const totalAmountValue = ref<string>('')
+const deductionAmountValue = ref<string>('')
+const takeHomeAmountValue = ref<string>('')
+const classificationValue = ref<string>('')
+const dialog = ref<boolean>(false)
+const dialogDelete = ref<boolean>(false)
+const labelList = ref<string[]>(['日付', '年齢', '業種', '総支給', '差引額', '手取り', '分類'])
+const keyList = ref<string[]>(['payment_date', 'age', 'industry', 'total_amount', 'deduction_amount', 'take_home_amount', 'classification'])
+const desserts = ref<string[] | number[]>([])
+const editedIndex = ref<number>(-1)
 // T.B.D
 // 現状は1にしておいて、後々ログイン画面作成時にパラメータでuser_idを取得出来るようにする
 const userId = ref<string>("1")
 
-// 仮状態の期間を変更した／していないのフラグ
-const isTmpStartDateChanged = ref<boolean>(false)
-const isTmpEndDateChanged = ref<boolean>(false)
+interface AnnualIncomeItem {
+  label: string
+  vModel: Ref<string | number>
+}
 
-// メニューが表示されているなら true
-const isMenuOpened = ref<boolean>(false)
+interface Headers {
+    title: string, 
+    key: string, 
+    sortable: boolean
+}
+
+interface Item {
+    'payment_date': string,
+    'age': number,
+    'industry': string,
+    'total_amount': number,
+    'deduction_amount': number,
+    'take_home_amount': number,
+    'classification': string,
+
+}
+
+const headers = ref<Headers[]>([
+    {
+        title: labelList.value[0],
+        key: keyList.value[0],
+        sortable: false
+    },
+    {
+        title: labelList.value[1],
+        key: keyList.value[1],
+        sortable: false
+    },
+    {
+        title: labelList.value[2],
+        key: keyList.value[2],
+        sortable: false
+    },
+    {
+        title: labelList.value[3],
+        key: keyList.value[3],
+        sortable: false
+    },
+    {
+        title: labelList.value[4],
+        key: keyList.value[4],
+        sortable: false
+    },
+    {
+        title: labelList.value[5],
+        key: keyList.value[5],
+        sortable: false
+    }
+])
+
+
+const annualIncomeList = ref<AnnualIncomeItem[]>([
+  {
+    label: labelList.value[0],
+    vModel: paymentDateValue,
+  },
+  {
+    label: labelList.value[1],
+    vModel: ageValue,
+  },
+  {
+    label: labelList.value[2],
+    vModel: industryValue,
+  },
+  {
+    label: labelList.value[3],
+    vModel: totalAmountValue,
+  },
+  {
+    label: labelList.value[4],
+    vModel: deductionAmountValue,
+  },
+  {
+    label: labelList.value[4],
+    vModel: takeHomeAmountValue,
+  },
+  {
+    label: labelList.value[5],
+    vModel: classificationValue,
+  },
+])
+
+const editedItem = ref<Item>({
+    payment_date: '',
+    age: 0,
+    industry: '',
+    total_amount: 0,
+    deduction_amount: 0,
+    take_home_amount: 0,
+    classification: '',
+})
+
+const defaultItem = ref<Item>({
+    payment_date: '',
+    age: 0,
+    industry: '',
+    total_amount: 0,
+    deduction_amount: 0,
+    take_home_amount: 0,
+    classification: '',
+})
 
 // メニューを閉じる。合わせて各状態を初期値に戻す
 const closeMenu = (): void => {
@@ -178,6 +398,52 @@ const handleUpdateEndDatepicker = (): void => {
   isTmpStartDateChanged.value = false
 }
 
+const formTitle = computed(() => editedIndex.value === -1 ? '新規登録' : '編集')
+
+const editItem = (item: Item): void => {
+    editedIndex.value = desserts.indexOf(item)
+    editedItem.value = Object.assign({}, item)
+    dialog.value = true
+}
+
+const deleteItem = (item: Item): void => {
+    editedIndex.value = desserts.indexOf(item)
+    editedItem.value = Object.assign({}, item)
+    dialogDelete.value = true
+}
+
+const deleteItemConfirm = (): void => {
+    desserts.splice(editedIndex.value, 1)
+    closeDelete()
+}
+
+const close = () => {
+  dialog.value = false
+  nextTick(() => {
+    editedItem.value = Object.assign({}, defaultItem())
+    editedIndex.value = -1
+  })
+}
+
+
+const closeDelete = () => {
+  dialog.value = false
+  nextTick(() => {
+    editedItem.value = Object.assign({}, defaultItem())
+    editedIndex.value = -1
+  })
+}
+
+const save = () => {
+  if (editedIndex.value > -1) {
+    Object.assign(desserts.value[editedIndex.value], editedItem.value);
+  } else {
+    desserts.value.push(editedItem.value);
+  }
+  close();
+};
+
+
 const  getRangeDateFetchData = async(): Promise<void> => {
     const queryList: string[] = []
     queryList.push("user_id=" + userId.value)
@@ -203,6 +469,14 @@ const  getRangeDateFetchData = async(): Promise<void> => {
 // ページ読み込み
 onMounted(() => {
     getRangeDateFetchData()
+})
+
+watch(dialog, (val: boolean): void => {
+    val || close()
+})
+
+watch(dialogDelete, (val: boolean) : void => {
+    val || dialogDelete()
 })
 
 </script>
