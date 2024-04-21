03657587b72aa2312cd67b3d8854ac4cde667277
hideto tsuzuki <ma_kux@icloud.com>
2024-04-21 12:46:51 +0900

    設定ファイル修正

--- jest.config.ts
+++ jest.config.ts
@@ -4,32 +4,19 @@ export default {
   transform: {
     '^.+\\.mjs$': 'babel-jest',
     '^.+\\.vue$': '@vue/vue3-jest',
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.ts$': 'babel-jest',
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
-  // transformIgnorePatterns: [
-  //   'node_modules/(?!vuetify)', // vuetifyを除外
-  // ],
   moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
-  // moduleNameMapper: {
-  //   '^@/(.*)$': '<rootDir>/src/$1',
-  // },
   "testEnvironmentOptions": {
     "customExportConditions": ["node", "node-addons"]
   },
-  setupFiles: ['./test/setting/jest.setup.ts'],
+  setupFiles: ['./tests/setting/jest.setup.ts'],
+  roots: [
+    "<rootDir>/src",
+    "<rootDir>/tests"
+  ],
+  testMatch: [
+    "**/tests/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
 };
-
-
-// export default {
-//   "roots": [
-//     "<rootDir>/src"
-//   ],
-//   "testMatch": [
-//     "**/__test__/**/*.+(ts|tsx|js)",
-//     "**/?(*.)+(spec|test).+(ts|tsx|js)"
-//   ],
-//   "transform": {
-//     "^.+\\.(ts|tsx)$": "ts-jest"
-//   },
-// };
